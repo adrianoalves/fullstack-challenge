fe807bacf9539bcd6fe960e5443998ec0d990e80
Adriano Alves <tohdi1@gmail.com>
2023-03-05 17:16:35 -0300

    - Config refinements:   - database and cache   - environment

--- api/config/database.php
+++ api/config/database.php
@@ -121,7 +121,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -139,7 +139,8 @@ return [
 
         'cache' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            # using the docker container name to ease config and deploy
+            'host' => env('REDIS_HOST', 'fs_redis'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),

2f302959d1bee4b86c0305712aa93ee10e8b52a6
Adriano Alves <tohdi1@gmail.com>
2023-03-07 01:24:46 -0300

    - Users Table.

--- frontend/src/components/UsersTable.vue
+++ frontend/src/components/UsersTable.vue
@@ -1,56 +1,79 @@
 <template>
   <div class="q-pa-sm">
     <q-table
-      title="Users"
+      title="Click/Tap to see User Weather Info"
+      ref="tableRef"
+      :loading="loading"
       :columns="columns"
       :rows="rows"
+      row-key="id"
+      v-model:pagination="pagination"
+      @request="onRequest"
+      @row-click="getWeather"
+      flat
+      bordered
     />
-    <div class="row justify-center q-mt-md">
-      <q-pagination
-        v-model="pagination.page"
-        color="grey-8"
-        :max="pagination.per_page"
-        size="sm"
-      />
-    </div>
+
   </div>
 </template>
 
 <script>
-import {defineComponent, ref, onMounted, reactive} from 'vue'
+import {defineComponent, ref, onMounted, computed, reactive} from 'vue'
 import {api} from "boot/axios";
 
 export default defineComponent({
   name: 'UsersTable',
   setup () {
+    const tableRef = ref()
     const columns = [
-      { name: 'name', align: 'center', label: 'User', field: 'name' },
-      { name: 'email', label: 'E-mail', field: 'email' }
+      { name: 'name', align: 'left', label: 'User', field: 'name' },
+      { name: 'email',align:'left', label: 'E-mail', field: 'email' }
     ]
-    let rows = reactive([])
-    let pagination = ref({
+    const loading = ref(false)
+    const rows = ref([])
+    const pagination = ref({
       page: 1,
-      per_page: 5
+      rowsPerPage: 5,
+      rowsNumber: null,
+      sortBy: 'desc',
+      descending: false
     })
 
     function getUsers (page, perPage) {
+      loading.value = true
       page = page || 1
       perPage = perPage || 5
       api.get(`/users?page=${page}&per_page=${perPage}`).then( response => {
         console.log( response.data );
-        rows = response.data.data
-        pagination.value.per_page = response.data.meta.per_page
+        rows.value = response.data.data
+        pagination.value.rowsNumber = response.data.meta.total
+        pagination.value.rowsPerPage = response.data.meta.per_page
         pagination.value.page = response.data.meta.current_page
+        loading.value = false
       });
+    }
+
+    function getWeather(evt, row) {
+      console.log('getting weather from user', row)
+    }
+
+    function onRequest( props ) {
+      console.log( props)
+      const { page, rowsPerPage } = props.pagination
+      getUsers(page, rowsPerPage)
     }
 
     onMounted( () => getUsers() )
 
     return {
+      tableRef,
+      loading,
       columns,
       pagination,
       rows,
-      getUsers
+      getUsers,
+      getWeather,
+      onRequest
     }
   }
 })
